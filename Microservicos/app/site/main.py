9aeec51c2d43f322fa3764456352dd9857d653e1
Alderano Fileni <73562784+afileni@users.noreply.github.com>
2020-12-19 18:02:34 -0300

    Update main.py

--- Microservicos/app/site/main.py
+++ Microservicos/app/site/main.py
@@ -97,7 +97,7 @@ def index():
 
             # Insert vote result into DB
             #vote = request.form['vote']
-            fname = request.form['vote']
+            fname = request.form['name']
             #lname = request.form['lname']
             #r.incr(vote,1)
             r.set(firstname, "kaskdlakd")
